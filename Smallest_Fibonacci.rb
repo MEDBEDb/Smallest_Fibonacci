0b0a2dc33412d6baf973ae69796c8627b2369f14
Sveatoslav Circel <MEDBEDb@me.com>
2013-05-09 04:25:12 +0300

    Create Smallest_Fibonacci.rb

---
+++ Smallest_Fibonacci.rb
@@ -0,0 +1,8 @@
+def F(n)
+if n < 3; 1; else; F(n-1)+F(n-2); end
+end
+
+puts "Enter how many first Fibonacci numbers you need:"
+x = gets.chomp
+puts "The first #{x} Fibonacci numbers are:" 
+(0..x.to_i).each {|n| print "#{F(n)}, "}
