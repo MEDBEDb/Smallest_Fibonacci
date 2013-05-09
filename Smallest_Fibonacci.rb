867409f5ef97488182c274011ac6691bf8c638c4
Sveatoslav Circel <MEDBEDb@me.com>
2013-05-09 22:25:14 +0300

    fixed zero element add +/- 1

--- Smallest_Fibonacci.rb
+++ Smallest_Fibonacci.rb
@@ -3,6 +3,6 @@ if n < 3; 1; else; F(n-1)+F(n-2); end
 end
 
 puts "Enter how many first Fibonacci numbers you need:"
-x = gets.chomp
-puts "The first #{x} Fibonacci numbers are:" 
+x = gets.chomp.to_i-1
+puts "The first #{x+1} Fibonacci numbers are:" 
 print "0 "; (1..x.to_i).each { |n| print "#{F(n)} "}
